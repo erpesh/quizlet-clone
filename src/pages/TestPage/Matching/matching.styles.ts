094212ceea3c583e475720cd21911394862aa98d
erpesh <89254828+erpesh@users.noreply.github.com>
2022-10-21 14:56:17 +0100

    matching component calculate order number done

--- src/pages/TestPage/Matching/matching.styles.ts
+++ src/pages/TestPage/Matching/matching.styles.ts
@@ -1,1 +1,49 @@
 import styled from "styled-components";
+import colors from "../../../assets/colors";
+
+export const Container = styled.article`
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    background-color: ${colors.whiteColor};
+    border-radius: 0.5rem;
+    box-shadow: 0 0.25rem 1rem 0 ${colors.shadowWithOpacity};
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-justify-content: space-between;
+    justify-content: space-between;
+    margin: 0 auto;
+    max-width: 52.5rem;
+    min-height: 29.25rem;
+    padding: 1.5rem 2rem;
+    -webkit-transition: all .5s ease;
+    transition: all .5s ease;
+    margin-top: 2rem;
+    position: relative;
+`
+export const ComponentTitle = styled.div`
+    color: ${colors.UIColorGray60};
+    font-size: .875rem;
+    font-weight: 600;
+    -webkit-letter-spacing: normal;
+    -moz-letter-spacing: normal;
+    -ms-letter-spacing: normal;
+    letter-spacing: normal;
+    line-height: 1.4285714285714286;
+    margin-bottom: 1.5rem;
+`
+export const ComponentHeader = styled.div`
+    color: ${colors.UIColorGray};
+    font-size: 1rem;
+    font-weight: 600;
+    -webkit-letter-spacing: normal;
+    -moz-letter-spacing: normal;
+    -ms-letter-spacing: normal;
+    letter-spacing: normal;
+    line-height: 1.5;
+    margin-bottom: 1.5rem;
+`
--- src/pages/TestPage/Matching/matching.tsx
+++ src/pages/TestPage/Matching/matching.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { testType } from '../interfaces';
+import {
+  NumberContainer,
+  NumberContent
+} from '../test-page.styles';
+import {
+  Container,
+  ComponentHeader,
+  ComponentTitle,
+} from './matching.styles';
 
+interface Props {
+  testSet: testType,
+  setTestSet: (testSet: testType) => void
+}
 
-const Matching = () => {
+const Matching: React.FC<Props> = ({ testSet, setTestSet }) => {
+
+  const calculateOrderNumber = (testData: testType) => {
+    const firstMatchingNumber = testSet.lengths.slice(0, 2).reduce((partialSum, a) => partialSum + a, 0);
+    const lastMatchingNumber = firstMatchingNumber + testSet.lengths[2];
+    const totalNumber = testSet.lengths.reduce((partialSum, a) => partialSum + a, 0);
+    return firstMatchingNumber + 1 + "-" + lastMatchingNumber + " of " + totalNumber;
+  }
+
   return (
-      <div>
-
-      </div>
+    <Container>
+      <ComponentTitle>Match term to definition</ComponentTitle>
+      <ComponentHeader>Select a term to match it with its definition</ComponentHeader>
+      <NumberContainer>
+        <NumberContent>{calculateOrderNumber(testSet)}</NumberContent>
+      </NumberContainer>
+    </Container>
   );
 };
 
