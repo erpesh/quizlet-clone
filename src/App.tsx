0ae153a2fdd26b1d5c1c7c206328862c8bfe4f5b
erpesh <89254828+erpesh@users.noreply.github.com>
2022-10-26 14:44:54 +0100

    pages lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
-import Home from "./pages/Home/home";
 import { Routes, Route } from "react-router-dom";
-import SetPage from "./pages/SetPage/set-page";
 import { AuthProvider } from "./context/auth-context";
-import CreateOrEdit from "./utils/CreateOrEdit/create-or-edit";
-import TestPage from "./pages/TestPage/test-page";
 import NavBar from './components/NavBar/nav-bar';
+import LoadingSpinner from './components/LoadingSpinner/loading-spinner';
 
+const Home = lazy(() => import("./pages/Home/home"));
+const SetPage = lazy(() => import("./pages/SetPage/set-page"));
+const TestPage = lazy(() => import("./pages/TestPage/test-page"));
+const CreateOrEdit = lazy(() => import("./utils/CreateOrEdit/create-or-edit"));
 
 function App() {
   return (
-      <div className="App">
-        <AuthProvider>
-          <NavBar/>
+    <div className="App">
+      <AuthProvider>
+        <NavBar />
+        <Suspense fallback={<LoadingSpinner />}>
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/create" element={<CreateOrEdit isCreate={true}/>}/>
-            <Route path="/:id" element={<SetPage/>}/>
-            <Route path="/:id/edit" element={<CreateOrEdit isCreate={false}/>}/>
-            <Route path="/:id/test" element={<TestPage/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateOrEdit isCreate={true} />} />
+            <Route path="/:id" element={<SetPage />} />
+            <Route path="/:id/edit" element={<CreateOrEdit isCreate={false} />} />
+            <Route path="/:id/test" element={<TestPage />} />
           </Routes>
-        </AuthProvider>
-      </div>
+        </Suspense>
+      </AuthProvider>
+    </div>
   );
 }
 
